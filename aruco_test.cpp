e9f31fc7dfca153ff476a69620d7d58b32044442
Quitino <fb_941219@163.com>
2021-12-07 17:32:33 +0800

    delete aruco lib

--- aruco_test.cpp
+++ aruco_test.cpp
@@ -3,32 +3,6 @@
 #include <opencv2/aruco.hpp>
 // #include <aruco/include/opencv2/aruco.hpp>
 
-// bool readDetectorParameters(std::string filename, cv::Ptr<cv::aruco::DetectorParameters> &params) {
-//     cv::FileStorage fs(filename, cv::FileStorage::READ);
-//     if(!fs.isOpened())
-//         return false;
-//     fs["adaptiveThreshWinSizeMin"] >> params->adaptiveThreshWinSizeMin;
-//     fs["adaptiveThreshWinSizeMax"] >> params->adaptiveThreshWinSizeMax;
-//     fs["adaptiveThreshWinSizeStep"] >> params->adaptiveThreshWinSizeStep;
-//     fs["adaptiveThreshConstant"] >> params->adaptiveThreshConstant;
-//     fs["minMarkerPerimeterRate"] >> params->minMarkerPerimeterRate;
-//     fs["maxMarkerPerimeterRate"] >> params->maxMarkerPerimeterRate;
-//     fs["polygonalApproxAccuracyRate"] >> params->polygonalApproxAccuracyRate;
-//     fs["minCornerDistanceRate"] >> params->minCornerDistanceRate;
-//     fs["minDistanceToBorder"] >> params->minDistanceToBorder;
-//     fs["minMarkerDistanceRate"] >> params->minMarkerDistanceRate;
-//     fs["cornerRefinementMethod"] >> params->cornerRefinementMethod;
-//     fs["cornerRefinementWinSize"] >> params->cornerRefinementWinSize;
-//     fs["cornerRefinementMaxIterations"] >> params->cornerRefinementMaxIterations;
-//     fs["cornerRefinementMinAccuracy"] >> params->cornerRefinementMinAccuracy;
-//     fs["markerBorderBits"] >> params->markerBorderBits;
-//     fs["perspectiveRemovePixelPerCell"] >> params->perspectiveRemovePixelPerCell;
-//     fs["perspectiveRemoveIgnoredMarginPerCell"] >> params->perspectiveRemoveIgnoredMarginPerCell;
-//     fs["maxErroneousBitsInBorderRate"] >> params->maxErroneousBitsInBorderRate;
-//     fs["minOtsuStdDev"] >> params->minOtsuStdDev;
-//     fs["errorCorrectionRate"] >> params->errorCorrectionRate;
-//     return true;
-// }
 
 int main(int argc, char *argv[])
 {
