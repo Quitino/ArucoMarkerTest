93f1ee765bf125e78d88daccdbf1213c4cc95d90
Quitino <fb_941219@163.com>
2021-12-07 17:47:02 +0800

    updata opencv-3.4.15

--- aruco_test.cpp
+++ aruco_test.cpp
@@ -1,19 +1,24 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <cv.h>
 #include <iostream>
-// #include <opencv2/aruco.hpp>
-#include <aruco/include/opencv2/aruco.hpp>
+#include <highgui.h>
+#include <opencv2/aruco.hpp>
+#include <stdio.h>
 
+using namespace cv;
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    
     // read param
-    // std::string filename = "../default.yaml";
-    std::string filename = "/home/q/develop/DemoSet/ArucoMarkerTest/default.yaml";
-    cv::FileStorage fs(filename, cv::FileStorage::READ);
+    std::string filename = "../default.yaml";
+    cv::FileStorage fs(filename, FileStorage::READ);
     if (!fs.isOpened()) 
     {
-        std::cout << "failed to open file " << filename << std::endl;
+        cout << "failed to open file " << filename << endl;
         return false;
     }
     double fx, fy, cx, cy, k1, k2, p1, p2, k3;
@@ -35,66 +40,47 @@ int main(int argc, char *argv[])
 
     std::cout<<"cameraMatrixK: "<<cameraMatrix<<std::endl;
 
-    // read video
-    std::string videoName = "/home/q/develop/DemoSet/ArucoMarkerTest/aruco_slam_data.mp4";
-    // std::string videoName = "../aruco_slam_data.mp4";
-    cv::VideoCapture capture(videoName);
+    VideoCapture capture("../aruco_slam_data.mp4");
     if(!capture.isOpened())
     {
-        std::cout<<"Movie open Error"<<std::endl;
+        cout<<"Movie open Error"<<endl;
         return -1;
     }
     double rate=capture.get(CV_CAP_PROP_FPS);
-    std::cout<<"FRAME_RATE:"<<" "<<rate<<std::endl;
-    std::cout<<"FRAME_COUNT:"<<" "<<capture.get(CV_CAP_PROP_FRAME_COUNT)<<std::endl;
-    cv::Mat frame;
+    cout<<"帧率为:"<<" "<<rate<<endl;
+    cout<<"总帧数为:"<<" "<<capture.get(CV_CAP_PROP_FRAME_COUNT)<<endl;
+    Mat frame;
     double position=0.0;
     capture.set(CV_CAP_PROP_POS_FRAMES,position);
 
-    // for aruco
-    // cv::Ptr<cv::aruco::DetectorParameters> detectorParams = cv::aruco::DetectorParameters::create();
-    // bool readOk = readDetectorParameters( filename, detectorParams);
-    // if(!readOk) {
-    //     std::cerr << "Invalid detector parameters file" << std::endl;
-    //     return 0;
-    // }
-    // override cornerRefinementMethod read from config file
-    // detectorParams->cornerRefinementMethod = 1;
-    // std::cout << "Corner refinement method (0: None, 1: Subpixel, 2:contour, 3: AprilTag 2): " << detectorParams->cornerRefinementMethod << std::endl;
-    // cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::generateCustomDictionary(30, 4);
-    // cv::Ptr<cv::aruco::Dictionary> dictionary =  cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
-    cv::aruco::Dictionary dictionary =  cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
-    // cv::aruco::Dictionary dictionary =  cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
-    long int count = 0;
-    while (capture.grab()) {
-        count ++;
-        cv::Mat image, imageCopy;
+   cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::generateCustomDictionary(30, 4);
+
+   while (capture.grab()) {
+       cv::Mat image, imageCopy;
         if(!capture.read(image))
             break;
-        image.copyTo(imageCopy);
-        std::vector<int> ids;
-        std::vector<std::vector<cv::Point2f>> corners;
+       image.copyTo(imageCopy);
+       std::vector<int> ids;
+       std::vector<std::vector<cv::Point2f>> corners;
 
-        // cv::aruco::detectMarkers(image, dictionary, corners, ids, detectorParams);
-        cv::aruco::detectMarkers(image, dictionary, corners, ids);
-        // if at least one marker detected
-        if (ids.size() > 0) {
-            cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
-            std::vector<cv::Vec3d> rvecs, tvecs;
-            cv::aruco::estimatePoseSingleMarkers(corners, 0.14, cameraMatrix, distCoeffs, rvecs, tvecs);
-            std::cout<<"R :"<<rvecs[0]<<std::endl;
-            std::cout<<"T :"<<tvecs[0]<<std::endl;
-            // draw axis for each marker
-            for(int i=0; i<ids.size(); i++)
-                cv::aruco::drawAxis(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
-        }else{
-            if( count%40 == 0)
-            std::cout<<"No marker detected!\n";
-        }
-        cv::imshow("out", imageCopy);
-        cv::waitKey(50);
-        //if (key == 27)1
-        // break;
-    }
+       cv::aruco::detectMarkers(image, dictionary, corners, ids);
+       // if at least one marker detected
+       if (ids.size() > 0) {
+           cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+           std::vector<cv::Vec3d> rvecs, tvecs;
+           cv::aruco::estimatePoseSingleMarkers(corners, 0.14, cameraMatrix, distCoeffs, rvecs, tvecs);
+           cout<<"R :"<<rvecs[0]<<endl;
+           cout<<"T :"<<tvecs[0]<<endl;
+           // draw axis for each marker
+           for(int i=0; i<ids.size(); i++)
+               cv::aruco::drawAxis(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
+       }else{
+           std::cout<<"No marker detected!\n";
+       }
+       cv::imshow("out", imageCopy);
+       cv::waitKey(50);
+       //if (key == 27)1
+       // break;
+   }
 return 0;
 }
